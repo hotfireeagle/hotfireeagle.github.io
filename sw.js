d4826e6dd4ce8bbcc0673b91322ca1b4778742c9
hotfireeagle <hotfireeagle@163.com>
2019-05-21 22:00:19 +0800

    Site updated: 2019-05-21 22:00:18

--- sw.js
+++ sw.js
@@ -24,15 +24,15 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2019/01/21/react遇上typescript/');
+  precacheUrls.push('/2018/12/12/node事件循环/');
 
   precacheUrls.push('/2019/04/26/轮播图实现/');
 
-  precacheUrls.push('/2019/05/07/FirstPaint/');
-
-  precacheUrls.push('/2019/05/16/利用原生JS实现简易的v-model/');
+  precacheUrls.push('/2019/05/16/typescript学习二/');
 
   precacheUrls.push('/2019/04/27/深入理解margin/');
+
+  precacheUrls.push('/2019/05/14/图片懒加载/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
