5b4e45b0b9f4cee8fd66346abf1c4b207cb0a727
hotfireeagle <hotfireeagle@163.com>
2019-11-10 23:57:15 +0800

    Site updated: 2019-11-10 23:57:13

--- sw.js
+++ sw.js
@@ -24,15 +24,15 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2018/11/26/react坑/');
+  precacheUrls.push('/2018/10/15/puppeteer-API备忘/');
 
-  precacheUrls.push('/2019/04/26/轮播图实现/');
+  precacheUrls.push('/2018/12/29/Express值请求体中间件解析/');
 
-  precacheUrls.push('/2019/05/16/typescript学习二/');
+  precacheUrls.push('/2019/01/11/Flutter之按钮/');
 
-  precacheUrls.push('/2019/04/27/深入理解margin/');
+  precacheUrls.push('/2019/10/03/History1/');
 
-  precacheUrls.push('/2019/06/05/Sequelize学习/');
+  precacheUrls.push('/2019/01/09/Flutter之文本/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
